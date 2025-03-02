9ec6198bbb54d05724118126dbbd049fc724e222
Julio Gonzales <dg2007xd@gmail.com>
2025-03-02 10:19:04 -0500

    add header

--- src/common/Header.jsx
+++ src/common/Header.jsx
@@ -1,60 +1,50 @@
+import { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import logo from '../assets/images/manutd.png';
+
 function Header() {
-    const location = useLocation();
+    const [scrolling, setScrolling] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 50) {
+                setScrolling(true);
+            } else {
+                setScrolling(false);
+            }
+        };
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
     return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary pt-2 fixed-top">
+        <nav className={`navbar navbar-expand-lg fixed-top ${scrolling ? 'bg-dark' : 'bg-transparent'}`}>
             <div className="container-fluid">
-                <Link className="navbar-brand" to="/"><img id='logo' src={logo} alt="" className='img-fluid' /></Link>
+                <Link className="navbar-brand" to="/">
+                    <img id='logo' src={logo} alt="Logo" className='img-fluid' />
+                </Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse justify-content-center" id="navbarTogglerDemo02">
                     <ul className="navbar-nav mx-auto">
-                        <li className="nav-item dropdown">
-                            <Link className="nav-link active ms-3 pages dropbtn" href="#scrollspyHeading1" aria-current="page" to="/">HOME {'>'}{'>'}</Link>
-                            <div className='dropdown-content'>
-                                <Link to="/">Home Club</Link>
-                                <Link to="#">Home School</Link>
-                                <Link to="#">Home Pub</Link>
-                                <Link to="#">Home Store</Link>
-                                <Link to="#">Home Club RTL</Link>
-                            </div>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <Link className="nav-link ms-3 pages dropbtn" href="#scrollspyHeading2" aria-current="page" to="/acercadenosotros">ACERCA DE NOSOTROS {'>'}{'>'}</Link>
-                            <div className='dropdown-content'>
-                                <Link to="/acercadenosotros">About Us</Link>
-                                <Link to="#">Our Team</Link>
-                                <Link to="#">Testimonials</Link>
-                                <Link to="#">FAQ</Link>
-                                <Link to="#">Gallery</Link>
-                            </div>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <Link className="nav-link ms-3 pages dropbtn" aria-current="page" to="#">PRODUCTOS {'>'}{'>'}</Link>
-                            <div className='dropdown-content'>
-                                <Link to="#">Shop</Link>
-                                <Link to="#">Wishlist</Link>
-                                <Link to="#">Cart</Link>
-                                <Link to="#">Checkout</Link>
-                                <Link to="#">My account</Link>
-                            </div>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <Link className="nav-link ms-3 pages dropbtn" aria-current="page" to="#">BLOG {'>'}{'>'}</Link>
-                            <div className='dropdown-content'>
-                                <Link to="#">Blog with Sidebar</Link>
-                                <Link to="#">Blog without Sidebar</Link>
-                            </div>
+                        <li className="nav-item">
+                            <Link className="nav-link active ms-3 pages" to="/">HOME</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link ms-3 pages" aria-current="page" to="/clientes">CLIENTES</Link>
+                            <Link className="nav-link ms-3 pages" to="/acercadenosotros">ABOUT US</Link>
                         </li>
-
-                        <li><Link className="nav-link ms-3 pages" aria-current="page" to="/contacto">CONTACTO</Link></li>
+                        <li className="nav-item">
+                            <Link className="nav-link ms-3 pages" to="#">PRODUCTS</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link ms-3 pages" to="#">BLOG</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link ms-3 pages" to="/clientes">CLIENTS</Link>
+                        </li>
+                        <li><Link className="nav-link ms-3 pages" to="/contacto">CONTACTS</Link></li>
                     </ul>
-
                     <ul className="navbar-nav mx-auto">
                         <li className='icono'>
                             <i className="bi bi-person-circle"></i>
@@ -69,7 +59,7 @@ function Header() {
                 </div>
             </div>
         </nav>
-    )
+    );
 }
 
-export default Header
+export default Header;
